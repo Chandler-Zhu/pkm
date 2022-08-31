d3653d4818da458d9768af5bdedb705a7ee7ee2d
Chandler <chandlerzhu68@gmail.com>
2022-08-31 04:14:58 -0400

    prevent text from wrapping in stats bar

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -21,7 +21,7 @@ const Pokemon = ({ pokemon }) => {
     pokemon.stats.map((stat, index) => (
       <div key={index} className="bg-slate-700 my-2 rounded p-1">
         <div
-          className="bg-slate-900 rounded px-2"
+          className="bg-slate-900 rounded px-2 whitespace-nowrap "
           style={{ width: `${stat.base_stat * 5}px` }}
         >
           {stat.stat.name}: {stat.base_stat}
